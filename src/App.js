98af557d5a4793a271caa2508f56f9f3ac4a45b7
Atropka <enbzhjd.vbfbk@yandex.ru>
2022-11-21 00:39:11 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Week from "./components/Week";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <nav>
+        <div className="work_item">Work Item</div>
+        <div className="tasks">
+          <div className="task">
+            <img src="/icons/1.svg" alt="1" /> Marketing launch
+          </div>
+          <div className="task">
+            <img src="/icons/1.svg" alt="1" />
+            Marketing launch
+          </div>
+          <div className="task">
+            <img src="/icons/1.svg" alt="1" />
+            Marketing launch
+          </div>
+          <div className="task">
+            <img src="/icons/1.svg" alt="1" />
+            Marketing launch
+          </div>
+          <div className="task">
+            <img src="/icons/1.svg" alt="1" />
+            Marketing launch
+          </div>
+          <div className="task">
+            <img src="/icons/1.svg" alt="1" />
+            Marketing launch
+          </div>
+        </div>
+      </nav>
+      <main>
+        <div className="table">
+          <div className="table_date">
+            <Week />
+            <Week />
+            <Week />
+            <Week />
+            <Week />
+            <Week />
+          </div>
+
+          <div className="table_overflow">
+            <div className="table_base"></div>{" "}
+            <div className="table_base"></div>
+            <div className="table_base"></div>{" "}
+            <div className="table_base"></div>
+            <div className="table_base"></div>{" "}
+            <div className="table_base"></div>
+            <div className="table_base"></div>{" "}
+            <div className="table_base"></div>
+            <div className="table_base"></div>{" "}
+            <div className="table_base"></div>
+            <div className="table_base"></div>{" "}
+            <div className="table_base"></div>
+            <div className="table_base"></div>{" "}
+            <div className="table_base"></div>
+            <div className="table_base"></div>{" "}
+            <div className="table_base"></div>
+            <div className="table_base"></div>{" "}
+            <div className="table_base"></div>
+            <div className="table_base"></div>{" "}
+            <div className="table_base"></div>
+            <div className="table_base"></div>{" "}
+            <div className="table_base"></div>
+            <div className="table_base"></div>{" "}
+            <div className="table_base"></div>
+            <div className="table_base"></div>{" "}
+            <div className="table_base"></div>
+            <div className="table_base"></div>{" "}
+            <div className="table_base"></div>
+            <div className="table_base"></div>{" "}
+            <div className="table_base"></div>
+            <div className="table_base"></div>{" "}
+            <div className="table_base"></div>
+            <div className="table_base"></div>{" "}
+            <div className="table_base"></div>
+            <div className="table_base"></div>{" "}
+            <div className="table_base"></div>
+            <div className="table_base"></div>{" "}
+            <div className="table_base"></div>
+            <div className="table_base"></div>{" "}
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
---
+++ src/components/Week.js
@@ -0,0 +1,18 @@
+export default function Week() {
+  return (
+    <>
+      <div className="part">
+        01 Sep - 07 Sep
+        <div className="days">
+          <div className="day">1</div>
+          <div className="day">2</div>
+          <div className="day">3</div>
+          <div className="day">4</div>
+          <div className="day">5</div>
+          <div className="day">6</div>
+          <div className="day">7</div>
+        </div>
+      </div>
+    </>
+  );
+}
